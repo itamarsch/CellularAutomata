d9fd24224329a7b368c88dc93faec79adb2c9203
Itamar Schwartz <itamarschwartz10@gmail.com>
2023-05-14 13:27:09 +0300

    Remove Scale 1 (-1)

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,13 @@ module Main (main) where
 
 import CellularAutomata (CellularAutomata (..))
 import CellularAutomata.Seeds (Seeds)
+import Control.Monad (join)
+import Data.Bifunctor (Bifunctor (bimap))
 import Data.List.Index (modifyAt)
 import Drawing (boardLines, boardSquares)
 import GHC.Float (float2Int)
 import Graphics.Gloss (Picture (Pictures, Translate), black, dark, green)
-import Graphics.Gloss.Interface.Pure.Game (Display (InWindow), Event (EventKey), Key (Char, MouseButton, SpecialKey), KeyState (Down), MouseButton (LeftButton), Picture (Scale), SpecialKey (KeyCtrlL, KeyEnter), play)
+import Graphics.Gloss.Interface.Pure.Game (Display (InWindow), Event (EventKey), Key (Char, MouseButton, SpecialKey), KeyState (Down), MouseButton (LeftButton), SpecialKey (KeyCtrlL, KeyEnter), play)
 import Grid (State (..), nextGrid)
 import Lib (cellSize, fps, horizontalCells, screenDimensions, verticalCells)
 
@@ -15,13 +17,12 @@ initialState = State {running = False, getGrid = replicate horizontalCells $ rep
 
 view :: (CellularAutomata a) => State a -> Picture
 view state =
-  Scale 1 (-1) $
-    Translate (-fst screenDimensions / 2) (-snd screenDimensions / 2) $
-      Pictures (boardLines (dark $ dark green) <> boardSquares (getGrid state))
+  Translate (-fst screenDimensions / 2) (-snd screenDimensions / 2) $
+    Pictures (boardLines (dark $ dark green) <> boardSquares (getGrid state))
 
 event :: (Ord a, CellularAutomata a) => Event -> State a -> State a
 event (EventKey (MouseButton LeftButton) Down _ (mouseX, mouseY)) state@(State _ grid) =
-  let (x, y) = (mouseX + (fst screenDimensions / 2), -mouseY + (snd screenDimensions / 2))
+  let (x, y) = (mouseX + (fst screenDimensions / 2), mouseY + (snd screenDimensions / 2))
    in state
         { getGrid =
             modifyAt (float2Int $ x / cellSize) (modifyAt (float2Int $ y / cellSize) nextValue) grid
@@ -40,4 +41,4 @@ main :: IO ()
 main = play (InWindow "Cellular" (both float2Int screenDimensions) (0, 0)) black fps (initialState :: State Seeds) view event periodic
 
 both :: (a -> b) -> (a, a) -> (b, b)
-both f (x, y) = (f x, f y)
+both = join bimap
